4725659c1c5bebe69b7cd3ec9f0f43589df53bba
Vithoba Shewale <shewvi@2020spaces.com>
2022-01-22 10:34:22 +0530

    Initial Setup

---
+++ calculator-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CalculatorApp from './CalculatorApp/CalculatorApp';
+
+ReactDOM.render(
+  <>
+    <CalculatorApp />
+  </>,
+  document.getElementById('root')
+);
